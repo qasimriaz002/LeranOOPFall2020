9704b31627e5260705ef2252a06a363050537ae8
Muhammad Qasim Riaz <qasim.riaz002@gmail.com>
2020-08-26 10:56:13 +0500

    This is 2nd Lecture: - In this lecture we will have a look at different kinds of data types in Python

---
+++ Lec2_DataTypes.py
@@ -0,0 +1,11 @@
+# In this lecture we will have a look at different kinds of data types in Python
+# Unlike other programming languages python dynamically assigns the data type to its variables
+
+x = 10              # Here python will make x an integer variable automatically
+y = 10.5            # Here python will make y a float variable automatically
+z = "Any Word"      # Here python will make z a string variable automatically
+
+# ---------------- New we can use type function of python to check the data type of variable
+print(type(x))
+print(type(y))
+print(type(z))
