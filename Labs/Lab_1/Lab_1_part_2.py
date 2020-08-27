051282babf98d14928077cf2ce5fc8b11d329070
DESKTOP-RB1OPIJ\qasim <qasim.riaz002@gmail.com>
2020-08-27 10:32:42 +0500

    This is 1st Lab: - Here we will use the dictionaries with the list - We will create the record of 5 students in the different 5 dictionaries - Then we will add all of the these three dictionaries in the list containing the record of all student

---
+++ Labs/Lab_1/Lab_1_part_2.py
@@ -0,0 +1,19 @@
+# Here we are going to learn the dictionary data type in python
+# We can say that it some what similar to strcutres in c++ but not 100% similar
+
+# Creating a dictionary datatype in python
+
+#               key   :   ans,   key : ans,   key   : ans
+studentDict_1 = {"name": "John", "age": 36, "marks": 40}
+print(studentDict_1)                     # printing a dictionary
+# Getting values from the dictionary using keys
+print("Name of student:")
+print(studentDict_1["name"])             # Getting value against the key -- name
+print("Age of student:")
+print(studentDict_1["age"])              # Getting value against the key -- age
+print("Marks of student:")
+print(studentDict_1["marks"])            # Getting value against the key -- marks
+
+
+
+
