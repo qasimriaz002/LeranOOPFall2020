9abdfa220d2ae3394b2e4a8dbe80acd531fe4f39
Muhammad Qasim Riaz <qasim.riaz002@gmail.com>
2020-09-01 10:07:25 +0500

    This is 3rd Lecture: - Here in this lecture we will have look on following data types --> Range --> Tuples --> Boolean

---
+++ Lectures/Lec3/Lec3_DataTypes.py
@@ -0,0 +1,16 @@
+# range data type in python
+
+x = range(2,10)
+print(x)
+print(type(x))
+
+#tuples data type in python
+
+mytuple = (40,50,34,66,21)
+print(mytuple)
+print(type(mytuple))
+
+# boolean data type in python
+
+myvariable1 = True
+print(type(myvariable1))
