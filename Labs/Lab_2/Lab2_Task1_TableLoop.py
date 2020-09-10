80685009b0bfb12bda8236d0118293b710079ab2
Muhammad Qasim Riaz <qasim.riaz002@gmail.com>
2020-09-10 08:29:03 +0500

    This is 2nd Lab:
    - Here in this lab I want you perform the tasks give in the committed files

---
+++ Labs/Lab_2/Lab2_Task1_TableLoop.py
@@ -0,0 +1,10 @@
+# Printing table using while loop
+print("----------------------")
+print("Table using while loop")
+print("----------------------")
+
+# Printing table using for loop
+print("----------------------")
+print("Table using For loop")
+print("----------------------")
+
---
+++ Labs/Lab_2/Lab2_Task2_EvenOdd.py
@@ -0,0 +1,11 @@
+# Taking number from user and checking if it is even or odd
+
+
+# Taking range from user and finding all the even and odd numbers present in that range
+# Using While Loop
+
+
+# Taking range from user and finding all the even and odd numbers present in that range
+# Using For Loop
+
+evenNumber = 4
